ccedcef13fc56a476a8d44122c77acd130087b4d
Lin Liu <lin_liu@listech.on.ca>
2022-01-13 15:12:56 -0500

    update

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,3 +1,8 @@
-export default function JokesIndex() {
-  return <div>testing</div>;
+export default function JokesIndexRoute() {
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+    </div>
+  );
 }
