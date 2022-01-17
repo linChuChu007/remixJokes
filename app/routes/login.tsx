cdda3d24ab4064072f3fc2922f308cd950951a41
Lin Liu <lin_liu@listech.on.ca>
2022-01-17 11:47:48 -0500

    update

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,8 @@
-import type { ActionFunction, LinksFunction } from "remix";
+import { ActionFunction, LinksFunction, redirect } from "remix";
 import { useActionData, json, Link, useSearchParams } from "remix";
 import { db } from "../../utils/db.server";
 import stylesUrl from "../styles/login.css";
+import { createUserSession, login } from "../../utils/session.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
@@ -64,10 +65,14 @@ export const action: ActionFunction = async ({ request }) => {
       // login to get the user
       // if there's no user, return the fields and a formError
       // if there is a user, create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
+      const user = await login({ username, password });
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Username/Password combination is incorrect.`,
+        });
+      }
+      return createUserSession(user.id, redirectTo);
     }
     case "register": {
       const userExists = await db.user.findFirst({
