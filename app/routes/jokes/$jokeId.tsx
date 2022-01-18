2a41c312e6c04513776dea00a9b9dd52b0b23295
Lin Liu <lin_liu@listech.on.ca>
2022-01-18 11:03:43 -0500

    add error boundary

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,5 +1,5 @@
 import type { LoaderFunction } from "remix";
-import { Link, useLoaderData } from "remix";
+import { Link, useLoaderData, useParams, useCatch } from "remix";
 import type { Joke } from "@prisma/client";
 import { db } from "../../../utils/db.server";
 
@@ -10,6 +10,11 @@ export const loader: LoaderFunction = async ({ params }) => {
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
   });
+  if (!joke) {
+    throw new Response("What a joke! Not found.", {
+      status: 404,
+    });
+  }
   if (!joke) throw new Error("Joke not found");
   const data: LoaderData = { joke };
   return data;
@@ -22,6 +27,27 @@ export default function JokeRoute() {
       <p>Here's your hilarious joke:</p>
       <p>{data.joke.content}</p>
       <Link to=".">{data.joke.name} Permalink</Link>
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  const params = useParams();
+  if (caught.status === 404) {
+    return (
+      <div className="error-container">
+        Huh? What the heck is "{params.jokeId}"{" "}
+      </div>
+    );
+  }
+  throw new Error(`Unhandled error: ${caught.status}`);
+}
+export function ErrorBoundary() {
+  const { jokeId } = useParams();
+  return (
+    <div className="error-container">
+      {`There was an error loading joke by the id ${jokeId}. Sorry.`}
     </div>
   );
 }
